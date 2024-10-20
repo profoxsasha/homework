c09c264091092850201971276123122d18762633
Your Name <you@example.com>
2024-10-20 20:09:11 +0300

    Olimpic squares

---
+++ main.py
@@ -0,0 +1,83 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+#
+# def print_hi(name):
+#     # Use a breakpoint in the code line below to debug your script.
+#     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+#
+#
+# # Press the green button in the gutter to run the script.
+# if __name__ == '__main__':
+#     print_hi('PyCharm')
+#
+# # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+import turtle
+turtle.color("blue")
+turtle.right(90)
+turtle.forward(90)
+turtle.right(90)
+turtle.forward(90)
+turtle.right(90)
+turtle.forward(90)
+turtle.right(90)
+turtle.forward(90)
+turtle.right(90)
+turtle.forward(60)
+
+turtle.speed(0)
+turtle.color("yellow")
+turtle.right(90)
+turtle.forward(30)
+turtle.left(90)
+turtle.forward(90)
+turtle.left(90)
+turtle.forward(90)
+turtle.left(90)
+turtle.forward(90)
+turtle.left(90)
+turtle.forward(60)
+turtle.right(180)
+turtle.forward(30)
+turtle.right(90)
+turtle.color("black")
+turtle.forward(30)
+turtle.left(90)
+turtle.forward(90)
+turtle.left(90)
+turtle.forward(90)
+turtle.left(90)
+turtle.forward(90)
+turtle.left(90)
+turtle.forward(90)
+turtle.left(90)
+turtle.forward(60)
+turtle.left(90)
+turtle.color("green")
+turtle.forward(30)
+turtle.right(90)
+turtle.forward(90)
+turtle.right(90)
+turtle.forward(90)
+turtle.right(90)
+turtle.forward(90)
+turtle.right(90)
+turtle.forward(90)
+turtle.right(90)
+turtle.forward(60)
+turtle.right(90)
+turtle.color("red")
+turtle.forward(30)
+turtle.left(90)
+turtle.forward(90)
+turtle.left(90)
+turtle.forward(90)
+
+turtle.left(90)
+turtle.forward(90)
+turtle.left(90)
+turtle.forward(90)
+turtle.mainloop()
