af5e7aa45508e1632a63ec3cb4d9c3c3b1851927
alex <izutovAlexander@gmail.com>
2024-12-01 15:19:30 +0300

    hw 01.12

--- main.py
+++ main.py
@@ -7,21 +7,44 @@ else:
     # name = "sasha"
     # age = 10
     # print("Hello my name is %s! i'm %d years old!"%(name,age))
+    #
+    # name1 = "sasha"
+    # age1 = 10
+    # name2 = "Vova"
+    # age2 = 10
+    # name3 = "Tima"
+    # age3 = 11
+    # print("name\t\tage\n%s\t\t%d\n%s\t\t%d\n%s\t\t%d"%(name1,age1,name2,age2,name3,age3))
 
-    name1 = "sasha"
-    age1 = 10
-    name2 = "Vova"
-    age2 = 10
-    name3 = "Tima"
-    age3 = 11
-    print("name\t\tage\n%s\t\t%d\n%s\t\t%d\n%s\t\t%d"%(name1,age1,name2,age2,name3,age3))
+    # a = int(input("введите число"))
+    # m = int(input("введите месец"))
+    # y = int(input("введите год"))
+    # if(a<=0 or a >31):
+    #     print("неверная дата")
+    # elif(m<=0 or m>12):
+    #     print("неверная дата")
+    # else:
+    #     print("%d.%d.%d"%(a,m,y))
+    # for u in range(1, 20):
+    #     print(u)
+    # print("четные")
+    # for u in range(1, 20):
+    #     if(u%2==0):
+    #         print(u)
+    # print("нечетные")
+    # for u in range(1, 20):
+    #     if(u%2!=0):
+    #         print(u)
 
-    a = int(input("введите число"))
-    m = int(input("введите месец"))
-    y = int(input("введите год"))
-    if(a<=0 or a >31):
-        print("неверная дата")
-    elif(m<=0 or m>12):
-        print("неверная дата")
-    else:
-        print("%d.%d.%d"%(a,m,y))
+     # r = int(input("введите число"))
+     # for t in range(1,11):
+     #     print("%d*%d=%d"%(t,r,t*r))
+    s = 0
+    r = int(input("введите число"))
+    s=r+s
+    while r != 0:
+        r = int(input("введите число"))
+        s=r+s
+    print("сумма %d"%(s))
+
+
